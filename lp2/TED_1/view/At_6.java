67b104e70fcc587f1b6af099ee9a512897939dc4
Williams Jose <moflin02@gmail.com>
2019-09-12 10:02:32 -0300

    Correção na At_6

--- lp2/TED_1/view/At_6.java
+++ lp2/TED_1/view/At_6.java
@@ -13,7 +13,11 @@ public class At_6 {
 		while(n != -1) {
 			System.out.println("Digite um número: ");
 			n = input.nextInt();
+			System.out.printf("O número digitado é: %s \n", (n % 2 == 0 ? "Par" : "Ímpar") );
+			System.out.println("----------------------");
 		}
+		
+		System.out.println("O programa foi finalizado!");
 		
 		input.close();
 
